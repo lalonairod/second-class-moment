47585eb56e606f08f96a6a8df6d853fb140e23ef
Eduardo Gonzalez <gonza.duardo98@gmail.com>
2022-09-21 22:31:13 -0500

    Example Moment.js

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'second-class-moment';
+
+  constructor(){
+    this.useMoment();
+  }
+
+  public useMoment(){
+    const TODAY = moment()
+    const YESTERDAY = TODAY.clone().subtract( 1, 'days')
+    const TOMORROW = moment().add( 1, 'days' )
+    const EXACTDATE = moment('2022-09-21')
+    const EXACTDATE1 = moment([ 2022, 0])
+    const MONTHDAYS = moment([2019, 1]).daysInMonth()
+
+    const FORMAT = 'dddd Do MMMM YYYY'
+
+    console.table({
+      Hoy : TODAY.format( FORMAT ),
+      Ayer : YESTERDAY.format( FORMAT ),
+      Mañana : TOMORROW.format( FORMAT ),
+      FechaExacta : EXACTDATE.format( FORMAT ),
+      FechaExacta1 : EXACTDATE1.format( FORMAT ),
+      DiasMes : MONTHDAYS,
+      Mayor : ( TODAY > YESTERDAY ) ? true : false
+    })
+  }
 }
